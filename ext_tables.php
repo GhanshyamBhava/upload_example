da0d01512701846c650a4e7214211f00674f7521
Ghanshyam Bhava <ghanshyambhava@yahoo.com>
2019-10-21 20:40:34 +0530

    Add wizard typoscript

--- ext_tables.php
+++ ext_tables.php
@@ -3,11 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    $_EXTKEY,
-    'Piexample',
-    'Upload Example'
-);
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Example Extension');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_uploadexample_domain_model_example', 'EXT:upload_example/Resources/Private/Language/locallang_csh_tx_uploadexample_domain_model_example.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:upload_example/Configuration/TSconfig/ContentElementWizard.ts">'
+);
